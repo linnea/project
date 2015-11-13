ad2430ae65c7dc56491a5e494d0b5df769b6e073
Linnea Watson <linneawatson@Linneas-MacBook-Pro-2.local>
2015-11-13 13:57:30 -0800

    Before cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,82 @@
+// Changes number of carousel elements based on viewport
+
 $(document).ready(function() {
-    $('.carousel').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+    var carousel = $('.carousel');
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 640) {
+        carousel.slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            prevArrow: $('.prev'),
+            nextArrow: $('.next')
+        });
+    } else if (viewportWidth < 1024) {
+        carousel.slick({
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            prevArrow: $('.prev'),
+            nextArrow: $('.next')
+        });
+    } else {
+        carousel.slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            prevArrow: $('.prev'),
+            nextArrow: $('.next')
+        });
+    }
+    var carouselResize = function(viewportWidth) {
+        if (viewportWidth < 640) {
+            carousel.slick('setOption', 'slidesToShow', 1);
+            carousel.slick('setOption', 'slidesToScroll', 1);
+        } else if (viewportWidth < 1024) {
+            carousel.slick('setOption', 'slidesToShow', 2);
+            carousel.slick('setOption', 'slidesToScroll', 2);
+        } else {
+            carousel.slick('setOption', 'slidesToShow', 3);
+            carousel.slick('setOption', 'slidesToScroll', 3);
+        }
+    };
+
+    $('.carousel--single').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         prevArrow: $('.prev'),
-        nextArrow: $('.next')
+        nextArrow: $('.next'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $(window).resize(function(viewportWidth) {
+        carouselResize($(window).width());
     });
 });
